a4d26bae1e739f963039fe7fbee27530bac723d5
Shivani Verma <shivani.vr.verma@gmail.com>
2025-12-07 15:09:09 +0530

    class extends

--- advance Js/classes oops/script.js
+++ advance Js/classes oops/script.js
@@ -34,8 +34,45 @@ class Pencile{
 }
 
 
-let pencil1 = new Pencile('Natraj', 'blue', 'Natraj Ltd', 10);
-let pencil2 = new Pencile('Apsara', 'red', 'Apsara Ltd', 12);
+// let pencil1 = new Pencile('Natraj', 'blue', 'Natraj Ltd', 10);
+// let pencil2 = new Pencile('Apsara', 'red', 'Apsara Ltd', 12);
 
-pencil1.write('Hello World in Blue');
-pencil2.write('Hello World in Red');
+// pencil1.write('Hello World in Blue');
+// pencil2.write('Hello World in Red');
+
+
+class User{
+    constructor(username ,email ,address ){
+        this.username = username;
+        this.email = email;
+        this.address = address;
+        this.role = 'user';
+    }
+
+    write(text){
+        let h1 = document.createElement('h1');
+        h1.innerHTML = `Username : ${this.username} </br> Message : ${text}`;
+        document.body.appendChild(h1);
+    }
+}
+
+class Admin extends User{
+    constructor(username,email ,address){
+        super(username ,email ,address);
+        this.role = 'admin';
+    }
+
+
+    deleteUser(){
+        document.querySelectorAll('h1').forEach(elem =>{
+            // if(elem.innerHTML.includes('username')){
+                elem.remove()
+
+        })
+    }
+}
+
+
+let user1 = new User('john deo','johndeo@john.com','UAS',);
+let user2 = new User('jane','jane@jane.com','India')
+let admin1 = new Admin('admin','admi@admin.com','india')
